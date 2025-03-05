20a47e190807c0b8b90d60b01fa308f724e19e48
Beppe99 <perroneg273@gmail.com>
2025-03-05 09:57:22 +0100

    Added the use of lxml to increase performance

--- VRAE/trajectory_converter.py
+++ VRAE/trajectory_converter.py
@@ -1,5 +1,5 @@
 import os
-import xml.etree.ElementTree as ET
+from lxml import etree
 import torch
 from torch.utils.data import Dataset
 from collections import defaultdict
@@ -27,34 +27,46 @@ class TrafficGenerativeDatasetTrajectory(Dataset):
     
     @staticmethod
     def _get_sequence_length(file_path):
-        tree = ET.parse(file_path)
-        root = tree.getroot()
-        vehicles = defaultdict(int)
         
-        for timestep in root.findall('timestep'):
-            for vehicle in timestep.findall('vehicle'):
-                vehicles[vehicle.get('id')] += 1
+        vehicles = defaultdict(int)
+        for event, timestep in etree.iterparse(file_path, tag="timestep"):
             
-        return max(vehicles.values())
+            seen_ids = {vehicle.get("id") for vehicle in timestep.findall("vehicle")}
+            for vid in seen_ids:
+                vehicles[vid] += 1
+            
+            timestep.clear()
+            while timestep.getprevious() is not None:
+             del timestep.getparent()[0]
+        
+        return max(vehicles.values(),default=0)
     
     @staticmethod
     def _process_file(args):
         file_path, max_length = args
-        tree = ET.parse(file_path)
-        root = tree.getroot()
         
         # Pre-allocazione dizionario per veicoli
         vehicles_data = defaultdict(lambda: np.zeros((max_length, 5)))
         vehicle_lengths = defaultdict(int)
         
         # Estrazione dati
-        for timestep in root.findall('timestep'):
+        for event, timestep in etree.iterparse(file_path, tag="timestep"):
             for vehicle in timestep.findall('vehicle'):
                 vid = vehicle.get('id')
                 idx = vehicle_lengths[vid] # 
                 if idx < max_length:
-                    vehicles_data[vid][idx] = np.array([float(vehicle.get('x')), float(vehicle.get('y')),float(vehicle.get('speed')),float(vehicle.get('angle')),float(vehicle.get('pos'))])
+                    vehicles_data[vid][idx] = np.array([
+                        float(vehicle.get('x')),
+                        float(vehicle.get('y')),
+                        float(vehicle.get('speed')),
+                        float(vehicle.get('angle')),
+                        float(vehicle.get('pos'))
+                        ])
                     vehicle_lengths[vid] += 1
+            
+            timestep.clear()
+            while timestep.getprevious() is not None:
+                del timestep.getparent()[0]                    
         
         # Converti in tensore
         sequences = [torch.tensor(data[:length], dtype=torch.float32) 
